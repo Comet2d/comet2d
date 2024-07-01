d142e08f93fe596bbb19ea59e94f66cada7c9f27
Ben <ben@dispairinggoose.co.uk>
2024-07-01 11:38:27 +0100

    add examples

---
+++ examples/2_texture_loading.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+const cmt = @import("comet");
+
+pub fn main() !void {
+    var gpa = (std.heap.GeneralPurposeAllocator(.{}){});
+    const allocator = gpa.allocator();
+
+    defer _ = gpa.deinit();
+    defer _ = gpa.detectLeaks();
+
+    var comet = try cmt.init(.{}, &allocator);
+    defer comet.quit();
+
+    // defaults to top left of texture
+    const heart = comet.graphics.loadTexture("assets/heart.png", .{});
+    defer heart.destroy();
+
+    // can provide an anchor position as either .start, .middle or .end, or as an absolute position
+    const heart_centered = comet.graphics.loadTexture("assets/heart.png", .{ .anchor_position = .{ .relative = .{ .x = .middle, .y = .middle } } });
+    // const heart_centered = comet.graphics.loadTexture("assets/heart.png", .{ .anchor_position = .{ .absolute = .{ .x = 4, .y = 4 } } });
+    defer heart_centered.destroy();
+
+    const heart_frames = comet.graphics.loadTexture("assets/heart_sheet.png", .{ .h_frames = 4 });
+    defer heart_frames.destroy();
+
+    // One texture can have any number of animators. The texture must live for at least as long as all of its animators, otherwise
+    // bad things will happen
+    var heart_animator = heart.createAnimator();
+    var heart_centered_animator = heart_centered.createAnimator();
+    var heart_frames_animator = heart_frames.createAnimator();
+
+    while (comet.nextFrame(16)) {
+        heart_animator.process(comet.time.integrated_delta);
+        heart_centered_animator.process(comet.time.integrated_delta);
+        heart_frames_animator.process(comet.time.integrated_delta);
+
+        comet.graphics.startFrame(.{ .r = 100, .g = 149, .b = 237 });
+
+        comet.graphics.draw(&heart_animator, .{ .x = 10, .y = 20 }, .{});
+        comet.graphics.draw(&heart_centered_animator, .{ .x = 20, .y = 20 }, .{});
+        comet.graphics.draw(&heart_frames_animator, .{ .x = 30, .y = 20 }, .{});
+
+        comet.graphics.endFrame();
+    }
+}
---
+++ examples/5_text.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const cmt = @import("comet"); // I call my import cmt to avoid name collisions with a comet handle
+
+pub fn main() !void {
+    // boilerplate for heap allocations
+    var gpa = (std.heap.GeneralPurposeAllocator(.{}){});
+    const allocator = gpa.allocator();
+
+    defer _ = gpa.deinit();
+    defer _ = gpa.detectLeaks();
+
+    // initialise comet
+    var comet = try cmt.init(.{}, &allocator);
+    defer comet.quit();
+
+    // load a font from a file
+    const font = comet.graphics.loadFont("assets/pixel.ttf", 8); // font size 10
+    defer font.close();
+
+    comet.graphics.setFont(&font);
+
+    // load a texture from a file.
+    const joke = comet.graphics.print("what do you call a man with a shovel on his head?", &cmt.graphics.Color.white, .{}) catch unreachable;
+    defer joke.destroy();
+
+    const punchline = comet.graphics.print("doug", &cmt.graphics.Color.white, .{}) catch unreachable;
+    defer punchline.destroy();
+
+    // create an animator for the texture. This is required to draw it to the screen.
+    var text_animator = joke.createAnimator();
+    var punchline_animator = punchline.createAnimator();
+
+    // next frame does a bunch of frame setup stuff, as well as delaying for, in this case, 16 milliseconds
+    // so you don't hit 100% CPU usage.
+    while (comet.nextFrame(16)) {
+        // process code goes here
+        text_animator.process(comet.time.integrated_delta);
+        punchline_animator.process(comet.time.integrated_delta);
+
+        comet.graphics.startFrame(.{ .r = 100, .g = 149, .b = 237 });
+
+        // drawing code goes here
+        comet.graphics.draw(&text_animator, .{ .x = 0, .y = 20 }, .{});
+        comet.graphics.draw(&punchline_animator, .{ .x = 0, .y = 36 }, .{});
+
+        comet.graphics.endFrame();
+    }
+}
